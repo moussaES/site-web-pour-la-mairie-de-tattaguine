be962e57bbda6ba57e603e0149a36b16b5df00c2
moussaES <faye70286@gmail.com>
2026-08-31 10:22:38 +0000

    fix: Create api/index.php entrypoint for Vercel Serverless Functions compatibility

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+// ====================================================================
+// POINT D'ENTRÉE SERVERLESS VERCEL (ROUTAGE VERS PUBLIC/INDEX.PHP)
+// ====================================================================
+
+require __DIR__ . '/../public/index.php';
